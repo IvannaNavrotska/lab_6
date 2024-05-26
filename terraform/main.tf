b68a8647ca5eae34b100bdad758a534f67327ffe
Ivanna Navrotska <navrockaivanna15@gmail.com>
2024-05-26 20:58:05 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,26 +25,26 @@ terraform {
   }
 }
 
-resource "aws_s3_bucket" "unique_s3_start" {
+resource "aws_s3_bucket" "my_s3_start" {
   provider = aws.localstack
-  bucket   = "unique-s3-start"
+  bucket   = "my-s3-start"
 }
 
-resource "aws_s3_bucket" "unique_s3_finish" {
+resource "aws_s3_bucket" "my_s3_finish" {
   provider = aws.localstack
-  bucket   = "unique-s3-finish"
+  bucket   = "my-s3-finish"
 }
 
-resource "aws_s3_bucket_acl" "unique_s3_start_acl" {
+resource "aws_s3_bucket_acl" "my_s3_start_acl" {
   provider = aws.localstack
-  bucket   = aws_s3_bucket.unique_s3_start.id
+  bucket   = aws_s3_bucket.my_s3_start.id
 
   acl = "private"
 }
 
-resource "aws_s3_bucket_lifecycle_configuration" "unique_s3_start_lifecycle" {
+resource "aws_s3_bucket_lifecycle_configuration" "my_s3_start_lifecycle" {
   provider = aws.localstack
-  bucket   = aws_s3_bucket.unique_s3_start.id
+  bucket   = aws_s3_bucket.my_s3_start.id
 
   rule {
     id     = "Move to Glacier"
@@ -57,9 +57,9 @@ resource "aws_s3_bucket_lifecycle_configuration" "unique_s3_start_lifecycle" {
   }
 }
 
-resource "aws_iam_role" "unique_lambda_exec_role" {
+resource "aws_iam_role" "my_lambda_exec_role" {
   provider = aws.localstack
-  name     = "unique_lambda_exec_role"
+  name     = "my_lambda_exec_role"
 
   assume_role_policy = jsonencode({
     Version   = "2012-10-17"
@@ -75,58 +75,58 @@ resource "aws_iam_role" "unique_lambda_exec_role" {
   })
 }
 
-resource "aws_lambda_function" "unique_s3_copy_lambda" {
+resource "aws_lambda_function" "my_s3_copy_lambda" {
   provider          = aws.localstack
   filename          = "lambda.zip"
-  function_name     = "unique-s3-copy-function"
-  role              = aws_iam_role.unique_lambda_exec_role.arn
+  function_name     = "my-s3-copy-function"
+  role              = aws_iam_role.my_lambda_exec_role.arn
   handler           = "lambda_function.lambda_handler"
   runtime           = "python3.8"
   source_code_hash  = filebase64sha256("lambda.zip")
 }
 
-resource "aws_lambda_permission" "unique_allow_s3_event" {
+resource "aws_lambda_permission" "my_allow_s3_event" {
   provider        = aws.localstack
   statement_id    = "AllowExecutionFromS3Bucket"
   action          = "lambda:InvokeFunction"
-  function_name   = aws_lambda_function.unique_s3_copy_lambda.function_name
+  function_name   = aws_lambda_function.my_s3_copy_lambda.function_name
   principal       = "s3.amazonaws.com"
-  source_arn      = aws_s3_bucket.unique_s3_start.arn
+  source_arn      = aws_s3_bucket.my_s3_start.arn
 }
 
-resource "aws_s3_bucket_notification" "unique_s3_start_notification" {
+resource "aws_s3_bucket_notification" "my_s3_start_notification" {
   provider = aws.localstack
-  bucket   = aws_s3_bucket.unique_s3_start.id
+  bucket   = aws_s3_bucket.my_s3_start.id
 
   lambda_function {
-    lambda_function_arn = aws_lambda_function.unique_s3_copy_lambda.arn
+    lambda_function_arn = aws_lambda_function.my_s3_copy_lambda.arn
     events              = ["s3:ObjectCreated:*"]
   }
 }
 
-resource "aws_sns_topic" "unique_example_topic" {
+resource "aws_sns_topic" "my_example_topic" {
   provider = aws.localstack
-  name     = "unique-example-topic"
+  name     = "my-example-topic"
 }
 
-resource "aws_sns_topic_subscription" "unique_example_subscription" {
+resource "aws_sns_topic_subscription" "my_example_subscription" {
   provider  = aws.localstack
-  topic_arn = aws_sns_topic.unique_example_topic.arn
+  topic_arn = aws_sns_topic.my_example_topic.arn
   protocol  = "lambda"
-  endpoint  = aws_lambda_function.unique_s3_copy_lambda.arn
+  endpoint  = aws_lambda_function.my_s3_copy_lambda.arn
 }
 
-resource "aws_s3_bucket_notification" "unique_s3_start_notification_with_sns" {
+resource "aws_s3_bucket_notification" "my_s3_start_notification_with_sns" {
   provider = aws.localstack
-  bucket   = aws_s3_bucket.unique_s3_start.id
+  bucket   = aws_s3_bucket.my_s3_start.id
 
   lambda_function {
-    lambda_function_arn = aws_lambda_function.unique_s3_copy_lambda.arn
+    lambda_function_arn = aws_lambda_function.my_s3_copy_lambda.arn
     events              = ["s3:ObjectCreated:*"]
   }
 
   topic {
-    topic_arn = aws_sns_topic.unique_example_topic.arn
+    topic_arn = aws_sns_topic.my_example_topic.arn
     events    = ["s3:ObjectCreated:*"]
   }
 }
